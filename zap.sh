90255f5cf9ed83d2c1d48ecdb7210e44ad4b3115
Omarrh49 <125512858+Omarrh49@users.noreply.github.com>
2024-05-27 12:10:21 +0100

    Update zap.sh

--- zap.sh
+++ zap.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# Ensure necessary permissions
+# Ensure the script has the correct executable permissions
 chmod 777 $(pwd)
 echo $(id -u):$(id -g)
 
 # Run ZAP API scan
-docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
+docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t http://localhost:5000/v3/api-docs -f openapi -r zap_report.html
 
 exit_code=$?
 
-# Move HTML Report
-sudo mkdir -p owasp-zap-report
-sudo mv zap_report.html owasp-zap-report
+# HTML Report
+mkdir -p owasp-zap-report
+mv zap_report.html owasp-zap-report
 
 echo "Exit Code : $exit_code"
 
 if [[ ${exit_code} -ne 0 ]]; then
-  echo "OWASP ZAP Report has either Low/Medium/High Risk. Please check the HTML Report"
-  exit 1
+    echo "OWASP ZAP Report has either Low/Medium/High Risk. Please check the HTML Report"
+    exit 1
 else
-  echo "OWASP ZAP did not report any Risk"
+    echo "OWASP ZAP did not report any Risk"
 fi
